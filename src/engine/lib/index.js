c3011779ac2aac3e5867bee17b627cd736be57f2
raficraft <raficraft@gmail.com>
2022-01-04 06:12:28 +0100

    finish landing page

--- src/engine/lib/index.js
+++ src/engine/lib/index.js
@@ -3,10 +3,3 @@ if (module.hot) {
     window.location.reload();
   });
 }
-
-import "./sass/engine.scss";
-import { UI } from "./js/Core/common/UI/UI";
-import { Guides } from "./js/Core/modules/Guides/Guides";
-
-new UI();
-new Guides();
